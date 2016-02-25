cd080038bdd02a83a9d447e93cf0d621f86fb5c2
Tyler Pinckard <tyjapi@aol.com>
2016-02-24 18:01:31 -0800

    Update install_manifold.sh

--- manifold-endpoint/install_manifold.sh
+++ manifold-endpoint/install_manifold.sh
@@ -5,10 +5,10 @@ mkdir /var/tmp/manifoldintstall
 cd /var/tmp/manifoldintstall
 
 curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u74-b02/jdk-8u74-linux-x64.rpm > jdk-8u74-linux-x64.rpm
-yum -y install jdk-8u74-linux-x64.rpm
+yum install -y jdk-8u74-linux-x64.rpm
 
-curl -X GET https://rpmhost.file.core.windows.net/rpm/rest-server-0.1-1.x86_64.rpm > rest-server-0.1-1.x86_64.rpm
-yum -y install rest-server-0.1-1.x86_64.rpm
+curl -X GET https://s3-us-west-2.amazonaws.com/rpmhost/rest-server-0.1-1.x86_64.rpm > rest-server-0.1-1.x86_64.rpm
+yum install -y rest-server-0.1-1.x86_64.rpm
 
 systemctl start rest-server
 cd
